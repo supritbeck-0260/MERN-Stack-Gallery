a0705151386023fb6a62b8d2419c76f8bc12198a
Suprit Beck <supritbeck@gmail.com>
2020-12-20 15:31:17 +0530

    Unfollow features added

--- server/Routes/mentor.js
+++ server/Routes/mentor.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Mentor = require('../Modals/mentor');
 const authorization = require('../middleware/Authorization');
 const profileInfo = require('../Modals/profileInfo');
-const { response } = require('express');
 
 router.post('/make',authorization, async (req,res)=>{
     try {
@@ -17,8 +16,17 @@ router.post('/make',authorization, async (req,res)=>{
                 uid:req.user._id,
                 date:Date.now()
             }
-            findMyMentor.mentoring.push(postToMyMentor);
-            findMyMentor.save();
+            const check = findMyMentor.mentoring.findIndex(value=>value.uid ==req.user._id);
+            if(check == -1){
+                findMyMentor.mentoring.push(postToMyMentor);
+                const length = findMyMentor.mentoring.length;
+                findMyMentor.save().then(response=>{
+                    res.json({mentoring:length});
+                });
+            }else{
+                const length = findMyMentor.mentoring.length;
+                res.json({mentoring:length});
+            }
         }else{
             res.status(201).json({message:'Faild to find your mentor data.'});
         }
@@ -29,16 +37,34 @@ router.post('/make',authorization, async (req,res)=>{
                 uid:mentorProfile._id,
                 date:Date.now()
             }
-            findMe.mentors.push(postToMe);
-            findMe.save().then(response=>{
-                res.json(response);
-            });
+            const check = findMe.mentors.findIndex(value=>value.uid ==req.user._id);
+            if(check == -1){
+                findMe.mentors.push(postToMe);
+                findMe.save(); 
+            }
         }else{
             res.status(201).json({message:'Faild to find your data.'});
         }
     } catch (error) {
         res.status(201).json(error); 
     }
+});
+
+router.post('/remove',authorization, async (req,res)=>{
+    // try {
+        const findMyMentor = await Mentor.findById({'_id':req.body.id});
+        const findMe = await Mentor.findById({'_id':req.user._id});
+        findMyMentor.mentoring = findMyMentor.mentoring.filter(value=>value.uid != req.user._id);
+        
+
+        const length = findMyMentor.mentoring.length;
+        findMyMentor.save();
+        findMe.mentors = findMe.mentors.filter(value=>value.uid != req.body.id);
+        findMe.save();
+        res.json({mentoring:length});
+    // } catch (error) {
+    //    res.status(201).json(error); 
+    // }
 });
 
 module.exports = router;
