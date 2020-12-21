32102e5e546903b1a38c849983ad0a5b135621f9
Suprit Beck <supritbeck@gmail.com>
2020-12-21 23:23:02 +0530

    admin access for product

--- server/product/route.js
+++ server/product/route.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const Products = require('./schema');
+const User = require('../Modals/user');
 const authorization = require('../middleware/Authorization');
 router.post('/save',authorization, async (req,res)=>{
     try {
-        const {item,name,link} = req.body
-        const product =await Products.find();
-        if(product[0][item]){
-            const find =  product[0][item].findIndex(value=>value.name==name);
-            if(find == -1){
-                product[0][item].push({name,link}); 
+        const admin = await User.findById({'_id':req.user._id});
+        if(admin.email == 'supritbeck@gmail.com'){
+            const {item,name,link} = req.body
+            const product =await Products.find();
+            if(product[0][item]){
+                const find =  product[0][item].findIndex(value=>value.name==name);
+                if(find == -1){
+                    product[0][item].push({name,link}); 
+                }else{
+                    product[0][item][find]= {name,link};
+                }
+                product[0].save().then(response=>{
+                    res.json({message:'Saved to Database.'});
+                });
             }else{
-                product[0][item][find]= {name,link};
+                res.status(201).json({message:'Item is missing in database'});
             }
-            product[0].save().then(response=>{
-                res.json({message:'Saved to Database.'});
-            });
         }else{
-            res.status(201).json({message:'Item is missing in database'});
+            res.status(201).json({message:'You are not authorized'});
         }
     } catch (error) {
         res.status(201).json({message:'Failed to save Data.'}); 
