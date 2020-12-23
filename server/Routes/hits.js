e274dd541ab26eceab6029bcf9f02a4c031a8def
Suprit Beck <supritbeck@gmail.com>
2020-12-23 22:36:03 +0530

    bug fix for hit routes

--- server/Routes/hits.js
+++ server/Routes/hits.js
@@ -7,8 +7,18 @@ router.get('/', async (req,res)=>{
     const gather =await Upload.find();
         if(gather){
             const sorted = gather.sort((a,b)=>{
-                if(b.avgRate && a.avgRate){
-                  return (b.avgRate.rate-a.avgRate.rate);
+                if(b.avgRate || a.avgRate){
+                    if(b.avgRate && a.avgRate){
+                      return (b.avgRate.rate-a.avgRate.rate);
+                     }else if(b.avgRate == undefined && a.avgRate){
+                        return -1;
+                    }else if(b.avgRate && a.avgRate == undefined){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else{
+                    return 0;
                 }
             });
             const limited = sorted.slice(0,10);
